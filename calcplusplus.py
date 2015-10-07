de0de948df2c1bca5285ec78edd01a8bcc2d8787
mustafa abdulkarim mahdi <mabdulka@zeta40.aulas.gsyc.es>
2015-10-07 10:33:22 +0200

    calcplusplus.py corregida 2ª vez

--- calcplusplus.py
+++ calcplusplus.py
@@ -12,30 +12,30 @@ if __name__ == "__main__":
     calchija = calcoohija.CalculadoraHija()
     with open(sys.argv[1]) as fichero:
         reader = csv.reader(fichero)
-        for ListaNumeros in reader:
+        for ListaNums in reader:
 
-            Operacion = ListaNumeros.pop(0)
+            Operacion = ListaNums.pop(0)
 
             if Operacion == 'suma':
-                suma = calchija.plus(int(ListaNumeros[0]), int(ListaNumeros[1]))
-                for numero in ListaNumeros[2:]:
+                suma = calchija.plus(int(ListaNums[0]), int(ListaNums[1]))
+                for numero in ListaNums[2:]:
                     suma = calchija.plus(suma, int(numero))
                 print(suma)
 
             elif Operacion == 'resta':
-                resta = calchija.minus(int(ListaNumeros[0]), int(ListaNumeros[1]))
-                for numero in ListaNumeros[2:]:
+                resta = calchija.minus(int(ListaNums[0]), int(ListaNums[1]))
+                for numero in ListaNums[2:]:
                     resta = calchija.minus(resta, int(numero))
                 print(resta)
 
             elif Operacion == 'multiplica':
-                multi = calchija.multi(int(ListaNumeros[0]), int(ListaNumeros[1]))
-                for numero in ListaNumeros[2:]:
+                multi = calchija.multi(int(ListaNums[0]), int(ListaNums[1]))
+                for numero in ListaNums[2:]:
                     multi = calchija.multi(multi, int(numero))
                 print(multi)
 
             elif Operacion == 'divide':
-                div = calchija.div(int(ListaNumeros[0]), int(ListaNumeros[1]))
-                for numero in ListaNumeros[2:]:
+                div = calchija.div(int(ListaNums[0]), int(ListaNums[1]))
+                for numero in ListaNums[2:]:
                     div = calchija.div(div, int(numero))
                     print(div)
