25cac9472a38fb253c88659cf3fd582d06af336c
mustafa abdulkarim mahdi <mabdulka@zeta40.aulas.gsyc.es>
2015-10-07 10:16:29 +0200

    Corregido error calcplus.py

--- calcplus.py
+++ calcplus.py
@@ -7,13 +7,12 @@ import calcoohija
 
 if __name__ == "__main__":
     calchija = calcoohija.CalculadoraHija()
-    File = open('prueba.csv', 'r')
-    Lista = File.readlines() #Devuelve una lista con cada linea del fichero#
-    
+    File = open(sys.argv[1], 'r')
+    Lista = File.readlines()  # Devuelve una lista con cada linea del fichero
+
     for linea in Lista:
         ListaNumeros = linea.split(",")
         Operacion = ListaNumeros.pop(0)
-
 
         if Operacion == 'suma':
             suma = calchija.plus(int(ListaNumeros[0]), int(ListaNumeros[1]))
@@ -22,7 +21,7 @@ if __name__ == "__main__":
             print(suma)
 
         elif Operacion == 'resta':
-            resta = calchija.minus(int(ListaNumeros[0]), int(ListaNumeros[1]))        
+            resta = calchija.minus(int(ListaNumeros[0]), int(ListaNumeros[1]))
             for numero in ListaNumeros[2:]:
                 resta = calchija.minus(resta, int(numero))
             print(resta)
@@ -38,5 +37,3 @@ if __name__ == "__main__":
             for numero in ListaNumeros[2:]:
                 div = calchija.div(div, int(numero))
                 print(div)
-
-
